dfa7a333a23d97085f7854a706ce49df67627dcc
NULL-OPERATOR <x@cryptolab.net>
2016-01-16 17:04:46 +0000

    fix coveralls setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,10 @@
 # users commonly want.
 #
 require 'coveralls'
-Coveralls.wear!
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 
+  Coveralls.wear!
 
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
