4f92ec44835dc8f1c989cdf91bf501586154f11d
NULL-OPERATOR <x@cryptolab.net>
2016-01-16 19:48:20 +0000

    adding discounts

--- lib/till.rb
+++ lib/till.rb
@@ -1,16 +1,14 @@
-require "oj"
 require "json"
 
 class Till
-  attr_reader :menu, :order, :names
-  TAX = 8.64
+  attr_reader :menu, :order, :names, :discounts, :sub_total
+  TAX = 0.0864
+
   def initialize(input)
     json_input = input || File.read("hipstercoffee.json")
     @input =  (JSON.parse(json_input)).reduce
     @menu = @input["prices"][0]
-    @order = {}
-    @sub_total = []
-    @names = []
+    @order, @sub_total, @discounts, @names = {}, 0, 0, []
   end
 
   def show_menu
@@ -22,27 +20,39 @@ class Till
     add_price(item, quantity)
   end
 
-  def add_price(item, quantity)
-    @sub_total << @menu[item] * quantity
-  end
-
   def add_name(name="")
     @names << name
   end
 
   def receipt
-    bill ={timeDate: time_date,
+    bill ={total: total,
+           timeDate: time_date,
            shopName: @input["shopName"],
            address: @input["address"],
            phone: @input["phone"],
            names: @names,
            orderList: order_list.to_h,
-           total: total}
+           discounts: @discounts,
+           tax: tax,
+           subTotal: @sub_total}
     File.open("bill.json","w"){|f| f << JSON.pretty_generate(bill) }
     JSON.pretty_generate(bill)
   end
 
   private
+
+  def add_price(item, quantity)
+    @sub_total += (@menu[item] * quantity)
+    food_discount(item)
+  end
+
+  def food_discount(food)
+    @discounts += food.include?("Muffin") ? @menu[food] * 0.1 : 0
+  end
+
+  def bill_discount
+    @sub_total >= 50 ? @discounts += @sub_total * 0.05 : 0
+  end
 
   def time_date
     Time.now.asctime
@@ -50,11 +60,16 @@ class Till
 
   def order_list
     @order.map do |item,amount|
-    [item, (amount.to_s+" x " + "#{@menu[item]}")]
+    [item, (amount.to_s + " x " + "#{@menu[item]}")]
     end
   end
 
+  def tax
+    @sub_total * TAX
+  end
+
   def total
-    @sub_total.reduce(:+)
+    bill_discount
+    @sub_total + tax - @discounts
   end
 end
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -7,9 +7,9 @@ describe Till do
   let(:menu_items) { [{shopName:"oh",
                        address:"12",
                        phone:"007",
-                       prices:[{Coffee: 4.75}]}].to_json }
+                       prices:[{Coffee: 4.75, Muffin: 1}]}].to_json }
   # let(:parsed_items) { (JSON.parse(menu_items)).reduce }
-  let(:menu_output) { {"Coffee"=>4.75} }
+  let(:menu_output) { {"Coffee"=>4.75, "Muffin"=>1} }
   let(:time_and_date) { Time.now.asctime }
 
   it "shows a list of menu items" do
@@ -20,7 +20,7 @@ describe Till do
     till.add_order("Coffee", 2)
   end
 
-  context "Ordering:" do
+  context "Ordering: " do
     it "adds an order" do
       expect(till.order["Coffee"]).to eq(2)
     end
@@ -36,8 +36,8 @@ describe Till do
     end
   end
 
-  context "Outputting the receipt" do
-    it "lists the items ordered" do
+  context "Outputting the receipt: " do
+    it "lists the item ordered and price" do
       expect(till.receipt).to include("Coffee" && "2 x 4.75")
     end
 
@@ -45,12 +45,29 @@ describe Till do
       expect(till.receipt).to include("oh" && "007")
     end
 
-    it "outputs a total" do
-      expect(till.receipt).to include("total" && "9.5")
+    it "outputs a sub-total" do
+      expect(till.receipt).to include("subTotal" && "9.5")
+    end
+
+    it "outputs a total plus tax" do
+      expect(till.receipt).to include("total" && "10.3208")
     end
 
     it "shows the current time and date" do
       expect(till.receipt).to include(time_and_date)
+    end
+  end
+
+  context "Calculating discounts: " do
+    it "calculates muffin discount at 10%" do
+      till.add_order("Muffin")
+      expect(till.discounts).to eq(0.1)
+    end
+
+    it "calculates over $50 discount at 5%" do
+      till.add_order("Coffee", 10)
+      till.receipt
+      expect(till.discounts).to eq(2.85)
     end
   end
 
